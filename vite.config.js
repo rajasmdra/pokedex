60ca8b8fa43fbb491a79cf8def17f12f8f8afe85
Raja Samudra Tandepadang <raja.t.padang@gmail.com>
2026-03-18 18:45:05 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Pokedex-react/',
+  base: '/pokedex/',
   plugins: [
     react(),
     tailwindcss()],
